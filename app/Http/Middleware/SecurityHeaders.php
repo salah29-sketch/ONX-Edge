6469312fab4157f6373ea71a81c779c3effcd72d
salah hamzaoui <hamzaouisalah29@gmail.com>
2026-06-03 10:44:19 +0100

    fix: add YouTube frame-src to CSP

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -10,7 +10,7 @@ class SecurityHeaders
         $response->headers->set('X-Frame-Options', 'SAMEORIGIN');
         $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
         $response->headers->set('Referrer-Policy', 'strict-origin-when-cross-origin');
-        $response->headers->set('Content-Security-Policy', "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https:; style-src 'self' 'unsafe-inline' https:; img-src 'self' data: https:; font-src 'self' https: data:; connect-src 'self' https:; frame-ancestors 'self';");
+        $response->headers->set('Content-Security-Policy', "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https:; style-src 'self' 'unsafe-inline' https:; img-src 'self' data: https:; font-src 'self' https: data:; connect-src 'self' https:; frame-ancestors 'self'; frame-src 'self' https://www.youtube.com https://youtube.com;");
         return $response;
     }
 }
